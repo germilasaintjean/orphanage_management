592d909e957850f895897e46dbab43d2505ff38e
root <root@WINDOWS-KEVJ6JC.localdomain>
2019-05-17 11:16:18 -0500

    readme

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # README
 
- Orphanage Management
+ Orphanage Management project
  
 This README would normally document whatever steps are necessary to get the
 application up and running.
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,6 @@
 Rails.application.routes.draw do
 
-  post 'workshops/add_person'
-  get 'workshops/search_person'
-  
-get 'workshops/generate_xl_sheet'
-post 'workshops/create_xl'
+
   # get 'workshop/edit'
 
   # get 'workshop/update'
@@ -20,15 +16,7 @@ post 'workshops/create_xl'
   root :to => 'home#index'
   resources :registrations
 
-   get 'prints/registration_card_print'
-   get 'prints/income_certificate_print'
-   get 'prints/community_certificate_print'
-   get 'prints/disability_certificate_print'
-  # post 'prints/search_person_details'
-   get 'print_preview/registration_card_print'
-   get 'print_preview/income_certificate_print'
-   get 'print_preview/community_certificate_print'
-   get 'print_preview/disability_certificate_print'
+
 
 get '/login' => 'sessions#new'
   post '/login' => 'sessions#create'
